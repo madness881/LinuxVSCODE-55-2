b073a4ee0db62846a22d7e91d0a1dc3c96e178de
madness881 <imrantasbaev84@gmail.com>
2025-08-01 05:59:42 +0000

    added:variant2 homework4

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import flet as ft 
+import flet as ft
 from datetime import datetime
+import random
 
 def main(page: ft.Page):
-    # page.add(ft.Text("Hello world"))
     page.title = 'Мое первое приложение на Flet'
     page.theme_mode = ft.ThemeMode.LIGHT
 
@@ -11,46 +11,32 @@ def main(page: ft.Page):
     greeting_history = []
     history_text = ft.Text("История приветствий:")
 
+    names_list = ["Алексей", "Мария", "Иван", "Ольга", "Сергей", "Наталья", "Дмитрий", "Екатерина"]
+
     def on_button_click(_):
         name = name_input.value.strip()
-
         now = datetime.now()
         hour = now.hour
 
-
-        if 6 <= hour <=12:
+        if 6 <= hour < 12:
             greeting_text.value = f"Good morning, {name}!"
-    
-            name_input.value = '  '
-
-
-            # timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            # greeting_history.append(f'{timestamp}---{name}')
-            # history_text.value = 'History username:\n' + '\n'.join(greeting_history)
-        
-        elif 12 <= hour <= 18:
-            greeting_text.value = f'Good day, {name}'
-            
-            name_input.value = ' '
-        
+        elif 12 <= hour < 18:
+            greeting_text.value = f"Good day, {name}"
         elif 18 <= hour < 24:
-            greeting_text.value = f'Good evening, {name}'
-            name_input.value = ' '
-        
+            greeting_text.value = f"Good evening, {name}"
         else:
-            greeting_text.value = "Good night! Please enter a name" 
+            greeting_text.value = "Good night! Please enter a name"
 
         greet_button.text = "Отправить еще раз"
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        greeting_history.append(f'{timestamp}---{name}')
-        history_text.value = 'История:\n' + '\n'.join(greeting_history) 
-        # print(greeting_text.value)
+        greeting_history.append(f'{timestamp} — {name}')
+        history_text.value = 'История:\n' + '\n'.join(greeting_history)
+
+        name_input.value = ''
         page.update()
-    
+
     def clear_history(_):
-        print("Test")
         greeting_history.clear()
-        print(f"История приветствий очищена. {greeting_history}")
         history_text.value = "История приветствий:"
         page.update()
 
@@ -60,18 +46,26 @@ def main(page: ft.Page):
         else:
             page.theme_mode = ft.ThemeMode.LIGHT
         page.update()
-            
+
+    def fill_random_name(_):
+        name_input.value = random.choice(names_list)
+        page.update()
+
+    # Кнопки
     theme_button = ft.IconButton(icon=ft.Icons.BRIGHTNESS_6, tooltip="Сменить тему", on_click=theme_mod)
     clear_button = ft.IconButton(icon_color=ft.Colors.GREEN, icon=ft.Icons.DELETE_FOREVER, tooltip="Очистить историю", on_click=clear_history)
 
-    name_input = ft.TextField(label="Plaase enter a name:✍🏻", on_submit=on_button_click)
+    random_name_button = ft.IconButton(icon=ft.Icons.SHUFFLE, tooltip="Случайное имя", on_click=fill_random_name)
+
+    name_input = ft.TextField(label="Пожалуйста, введите имя: ✍🏻", on_submit=on_button_click)
     greet_button = ft.ElevatedButton("Отправить", on_click=on_button_click, icon=ft.Icons.SEND)
-    # greet_button_1 = ft.TextButton("Отправить", on_click=on_button_click, icon=ft.Icons.SEND)
 
-    # page.add(theme_button, greeting_text, name_input, greet_button, clear_button, history_text)
-
-    page.add(ft.Row([theme_button, clear_button], alignment=ft.MainAxisAlignment.SPACE_BETWEEN), 
-             greeting_text, 
-             ft.Row([name_input, greet_button, history_text], alignment=ft.MainAxisAlignment.CENTER))
+    # Добавление элементов на страницу
+    page.add(
+        ft.Row([theme_button, clear_button, random_name_button], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
+        greeting_text,
+        ft.Row([name_input, greet_button], alignment=ft.MainAxisAlignment.CENTER),
+        history_text
+    )
 
 ft.app(target=main, view=ft.WEB_BROWSER)
